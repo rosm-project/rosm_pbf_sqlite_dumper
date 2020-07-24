7cf16d1afcda5b4fdc6d9fc2ad3c6a1dbca2571c
Zsolt Bölöny <bolony.zsolt@gmail.com>
2020-07-24 13:14:27 +0200

    Rusqlite import improvements

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rosm_pbf_reader::{PbfReader, Block, TagReader, DeltaValueReader, DenseNodeRe
 use rosm_pbf_reader::pbf;
 use rosm_pbf_reader::util::*;
 
-use rusqlite as sql;
+use rusqlite::{NO_PARAMS, params, Transaction};
 
 use std::fs::File;
 
@@ -12,13 +12,13 @@ use config::{Config, read_config};
 mod error;
 use error::DumperError;
 
-fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
+fn create_tables(tr: &Transaction) -> rusqlite::Result<()> {
     tr.execute(
         "CREATE TABLE header (
             key TEXT,
             value TEXT
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE nodes (
@@ -26,7 +26,7 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             lat INTEGER NOT NULL,
             lon INTEGER NOT NULL
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE node_tags (
@@ -35,7 +35,7 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             value TEXT,
             FOREIGN KEY(node_id) REFERENCES nodes(id)
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE node_info (
@@ -47,13 +47,13 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             visible BOOL,
             FOREIGN KEY(node_id) REFERENCES nodes(id)
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE ways (
             id INTEGER PRIMARY KEY
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE way_tags (
@@ -62,7 +62,7 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             value TEXT,
             FOREIGN KEY(way_id) REFERENCES ways(id)
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE way_info (
@@ -74,7 +74,7 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             visible BOOL,
             FOREIGN KEY(way_id) REFERENCES ways(id)
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE way_refs (
@@ -83,13 +83,13 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             FOREIGN KEY(way_id) REFERENCES ways(id),
             FOREIGN KEY(ref_node_id) REFERENCES nodes(id) DEFERRABLE INITIALLY DEFERRED
         )", 
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE relations (
             id INTEGER PRIMARY KEY
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE relation_members (
@@ -100,7 +100,7 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             role TEXT,
             FOREIGN KEY(relation_id) REFERENCES relations(id)
         )", 
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE relation_tags (
@@ -109,7 +109,7 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             value TEXT,
             FOREIGN KEY(relation_id) REFERENCES relations(id)
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     tr.execute(
         "CREATE TABLE relation_info (
@@ -121,47 +121,47 @@ fn create_tables(tr: &sql::Transaction) -> sql::Result<()> {
             visible BOOL,
             FOREIGN KEY(relation_id) REFERENCES relations(id)
         )",
-        sql::params![],
+        NO_PARAMS,
     )?;
     Ok(())
 }
 
-fn process_header_block(block: pbf::HeaderBlock, tr: &sql::Transaction) -> sql::Result<()> {
+fn process_header_block(block: pbf::HeaderBlock, tr: &Transaction) -> rusqlite::Result<()> {
     let mut insert_info = tr.prepare_cached("INSERT INTO header (key, value) VALUES (?1, ?2)")?;
 
     if let Some(bbox) = &block.bbox {
-        insert_info.execute(sql::params!["bbox_left", bbox.left])?;
-        insert_info.execute(sql::params!["bbox_right", bbox.right])?;
-        insert_info.execute(sql::params!["bbox_top", bbox.top])?;
-        insert_info.execute(sql::params!["bbox_bottom", bbox.bottom])?;
+        insert_info.execute(params!["bbox_left", bbox.left])?;
+        insert_info.execute(params!["bbox_right", bbox.right])?;
+        insert_info.execute(params!["bbox_top", bbox.top])?;
+        insert_info.execute(params!["bbox_bottom", bbox.bottom])?;
     }
 
     for feature in &block.required_features {
-        insert_info.execute(sql::params!["required_feature", feature])?;
+        insert_info.execute(params!["required_feature", feature])?;
     }
 
     for feature in &block.optional_features {
-        insert_info.execute(sql::params!["optional_feature", feature])?;
+        insert_info.execute(params!["optional_feature", feature])?;
     }
 
     if let Some(writing_program) = &block.writingprogram {
-        insert_info.execute(sql::params!["writing_program", writing_program])?;
+        insert_info.execute(params!["writing_program", writing_program])?;
     }
 
     if let Some(source) = &block.source {
-        insert_info.execute(sql::params!["source", source])?;
+        insert_info.execute(params!["source", source])?;
     }
 
     if let Some(osmosis_replication_timestamp) = &block.osmosis_replication_timestamp {
-        insert_info.execute(sql::params!["osmosis_replication_timestamp", osmosis_replication_timestamp])?;
+        insert_info.execute(params!["osmosis_replication_timestamp", osmosis_replication_timestamp])?;
     }
 
     if let Some(osmosis_replication_sequence_number) = &block.osmosis_replication_sequence_number {
-        insert_info.execute(sql::params!["osmosis_replication_sequence_number", osmosis_replication_sequence_number])?;
+        insert_info.execute(params!["osmosis_replication_sequence_number", osmosis_replication_sequence_number])?;
     }
 
     if let Some(osmosis_replication_base_url) = &block.osmosis_replication_base_url {
-        insert_info.execute(sql::params!["osmosis_replication_base_url", osmosis_replication_base_url])?;
+        insert_info.execute(params!["osmosis_replication_base_url", osmosis_replication_base_url])?;
     }
 
     Ok(())
@@ -212,7 +212,7 @@ impl<'a> OsmPrimitive for DenseNode<'a> {
     }
 }
 
-fn insert_info<P: OsmPrimitive>(primitive: &P, block: &pbf::PrimitiveBlock, insert_stmt: &mut sql::CachedStatement) -> sql::Result<()> {
+fn insert_info<P: OsmPrimitive>(primitive: &P, block: &pbf::PrimitiveBlock, insert_stmt: &mut rusqlite::CachedStatement) -> rusqlite::Result<()> {
     if let Some(info) = primitive.info() {
         let user = if let Some(string_id) = info.user_sid {
             Some(std::str::from_utf8(block.stringtable.s[string_id as usize].as_ref()).unwrap())
@@ -226,12 +226,12 @@ fn insert_info<P: OsmPrimitive>(primitive: &P, block: &pbf::PrimitiveBlock, inse
             None
         };
 
-        insert_stmt.execute(sql::params![primitive.id(), info.version, timestamp, info.uid, user, info.visible])?;
+        insert_stmt.execute(params![primitive.id(), info.version, timestamp, info.uid, user, info.visible])?;
     }
     Ok(())
 }
 
-fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, config: &Config) -> sql::Result<()> {
+fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &Transaction, config: &Config) -> rusqlite::Result<()> {
     let mut insert_node = tr.prepare_cached("INSERT INTO nodes (id, lat, lon) VALUES (?1, ?2, ?3)")?;
     let mut insert_node_tag = tr.prepare_cached("INSERT INTO node_tags (node_id, key, value) VALUES (?1, ?2, ?3)")?;
     let mut insert_node_info = tr.prepare_cached("INSERT INTO node_info (node_id, version, timestamp, user_id, user, visible) VALUES (?1, ?2, ?3, ?4, ?5, ?6)")?;
@@ -255,7 +255,7 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
 
                 for node in nodes {
                     let coord = normalize_coord(node.lat, node.lon, &block);
-                    insert_node.execute(sql::params![node.id, coord.0, coord.1])?;
+                    insert_node.execute(params![node.id, coord.0, coord.1])?;
 
                     if !config.skip_node_info {
                         insert_info(&node, &block, &mut insert_node_info)?;
@@ -264,7 +264,7 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
                     if !config.skip_node_tags {
                         for (key, value) in node.tags {
                             if !config.skip_tag_keys.contains(key?) {
-                                insert_node_tag.execute(sql::params![node.id, key?, value?])?;
+                                insert_node_tag.execute(params![node.id, key?, value?])?;
                             }
                         }
                     }
@@ -272,14 +272,14 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
             } else {
                 for node in &group.nodes {
                     let coord = normalize_coord(node.lat, node.lon, &block);
-                    insert_node.execute(sql::params![node.id, coord.0, coord.1])?;
+                    insert_node.execute(params![node.id, coord.0, coord.1])?;
 
                     if !config.skip_node_tags {
                         let tags = TagReader::new(&node.keys, &node.vals, string_table);
 
                         for (key, value) in tags {
                             if !config.skip_tag_keys.contains(key?) {
-                                insert_node_tag.execute(sql::params![node.id, key?, value?])?;
+                                insert_node_tag.execute(params![node.id, key?, value?])?;
                             }
                         }
                     }
@@ -293,14 +293,14 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
 
         if !config.skip_ways {
             for way in &group.ways {
-                insert_way.execute(sql::params![way.id])?;
+                insert_way.execute(params![way.id])?;
 
                 if !config.skip_way_tags {
                     let tags = TagReader::new(&way.keys, &way.vals, string_table);
 
                     for (key, value) in tags {
                         if !config.skip_tag_keys.contains(key?) {
-                            insert_way_tag.execute(sql::params![way.id, key?, value?])?;
+                            insert_way_tag.execute(params![way.id, key?, value?])?;
                         }
                     }
                 }
@@ -313,7 +313,7 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
                     let refs = DeltaValueReader::new(&way.refs);
 
                     for node_id in refs {
-                        insert_way_ref.execute(sql::params![way.id, node_id])?;
+                        insert_way_ref.execute(params![way.id, node_id])?;
                     }
                 }
             }
@@ -321,14 +321,14 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
 
         if !config.skip_relations {
             for relation in &group.relations {
-                insert_relation.execute(sql::params![relation.id])?;
+                insert_relation.execute(params![relation.id])?;
 
                 if !config.skip_relation_tags {
                     let tags = TagReader::new(&relation.keys, &relation.vals, string_table);
 
                     for (key, value) in tags {
                         if !config.skip_tag_keys.contains(key?) {
-                            insert_relation_tag.execute(sql::params![relation.id, key?, value?])?;
+                            insert_relation_tag.execute(params![relation.id, key?, value?])?;
                         }
                     }
                 }
@@ -356,7 +356,7 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
                         let string_id = relation.roles_sid[i];
                         let role = std::str::from_utf8(string_table.s[string_id as usize].as_ref())?;
 
-                        insert_relation_member.execute(sql::params![relation.id, node_id, way_id, rel_id, role])?;
+                        insert_relation_member.execute(params![relation.id, node_id, way_id, rel_id, role])?;
                     }
                 }
             }
@@ -366,15 +366,15 @@ fn process_primitive_block(block: pbf::PrimitiveBlock, tr: &sql::Transaction, co
     Ok(())
 }
 
-fn dump<Input: std::io::Read>(pbf_reader: &mut PbfReader<Input>, conn: &mut sql::Connection, config: &Config) -> sql::Result<()> {
+fn dump<Input: std::io::Read>(pbf_reader: &mut PbfReader<Input>, conn: &mut rusqlite::Connection, config: &Config) -> rusqlite::Result<()> {
     {
         let tr = conn.transaction()?;
         create_tables(&tr)?;
         tr.commit()?;
     }
 
-    conn.execute("PRAGMA synchronous = OFF", sql::params![])?;
-    conn.query_row_and_then("PRAGMA journal_mode = MEMORY", sql::params![], |_row| -> sql::Result<()> {
+    conn.execute("PRAGMA synchronous = OFF", NO_PARAMS)?;
+    conn.query_row_and_then("PRAGMA journal_mode = MEMORY", NO_PARAMS, |_row| -> rusqlite::Result<()> {
         Ok(())
     })?;
 
@@ -410,7 +410,7 @@ fn main() -> Result<(), DumperError> {
             .map_err(|err| DumperError::new(err.into(), format!("Failed to remove `{:?}`", config.output_db)))?;
     }
 
-    let mut conn = sql::Connection::open(&config.output_db)
+    let mut conn = rusqlite::Connection::open(&config.output_db)
         .map_err(|err| DumperError::new(err.into(), format!("Failed to open output SQLite database `{:?}`", config.output_db)))?;
 
     dump(&mut reader, &mut conn, &config)
